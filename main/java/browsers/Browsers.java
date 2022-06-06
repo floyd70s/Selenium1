5f4ac53066b6717b1900e8a6b5ba633e74b33254
claudioperezdiaz <claudioperezdiaz@gmail.com>
2022-06-06 19:35:54 -0400

    first commit

---
+++ main/java/browsers/Browsers.java
@@ -0,0 +1,22 @@
+package browsers;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Browsers {
+    private WebDriver driver;
+    public ChromeDriver localChrome(){
+        try {
+            System.setProperty("webdriver.chrome.driver","/Users/claudioperez/Documents/programacion/chromeDriverSelenium");
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+
+            driver.get("https://www.google.cl");
+            Thread.sleep(5000);
+        }
+        catch (Exception e){
+            System.out.println("load error in driver"+e.getMessage());
+        }
+        return (ChromeDriver) driver;
+    }
+}
---
+++ main/java/browsers/Main.java
@@ -0,0 +1,8 @@
+package browsers;
+
+public class Main {
+    public static void main(String[] args){
+        Browsers b= new Browsers();
+        b.localChrome();
+    }
+}
